3d8a735399b3741e60120caadea24b92e5290815
Muflone <webreg@muflone.com>
2025-03-21 03:00:32 +0100

    Changed license

--- src/set_workspace.py
+++ src/set_workspace.py
@@ -1,20 +1,21 @@
 ##
-#   Project: wmectrl - An enhanced window manager control
-#    Author: Fabio Castelli <muflone@vbsimple.net>
-# Copyright: 2010 Fabio Castelli
-#   License: GPL-2+
-#  This program is free software; you can redistribute it and/or modify it
-#  under the terms of the GNU General Public License as published by the Free
-#  Software Foundation; either version 2 of the License, or (at your option)
-#  any later version.
-# 
-#  This program is distributed in the hope that it will be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-#  more details.
-# 
-# On Debian GNU/Linux systems, the full text of the GNU General Public License
-# can be found in the file /usr/share/common-licenses/GPL-2.
+#     Project: wmectrl
+# Description: An enhanced window manager control
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2010-2025 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
 import utils
